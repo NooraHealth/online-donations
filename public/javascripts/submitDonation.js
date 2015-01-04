723abbc734e9d050b2e27d34d7f68ddf3253d861
Lucy Richards <lucyannerichards@gmail.com>
2015-01-03 16:47:41 -0800

    charges are being made

--- public/javascripts/submitDonation.js
+++ public/javascripts/submitDonation.js
@@ -2,9 +2,6 @@
   $(document).ready(function() {
   
     function stripeResponseHandler(status, response){
-      console.log("Recieved a response from stripe: ");
-      console.log(status);
-      console.log(response);
       var form = $('#payment-form');
       
       if(response.error) {
@@ -13,7 +10,7 @@
       } else{
         var token = response.id;
         //insert the token into the form for submission
-        form.append("<input type='hidden' name='stripeToken' value="+token+"/>" );
+        form.append("<input type='hidden' name='stripeToken' value='"+token+"' />" );
         form.get(0).submit();
       }
     }
@@ -22,10 +19,13 @@
       e.preventDefault();
       var form = $(this);
       form.find('button').prop('disabled', true);
-      console.log("Getting a token from Stripe"); 
       Stripe.card.createToken(form, stripeResponseHandler);
       return false; 
     });
 
+    $('#donation-options').on('click', function(e) {
+      console.log("An option was selected");
+      console.log($(this));
+    });
   })
 }).call(this);
--- routes/donations.coffee
+++ routes/donations.coffee
@@ -1,6 +1,5 @@
-
 express = require('express')
-stripe = require('stripe')('pk_test__y4XPVW7eG98vkn2FgtsonNYt')
+stripe = require('stripe')('sk_test_ASzEwo4Y9IlE0M8gBrLkwrP0')
 router = express.Router()
 
 # GET home page. 
@@ -8,8 +7,24 @@ router.post '/submit', (req, res) ->
   console.log 'recieved a payment to submit'
   stripeToken = req.body.stripeToken
   donationAmount = req.body.donationAmount
+  email = req.body.email
+
   console.log stripeToken
   console.log donationAmount
+  stripe.charges.create {
+    amount: 1000
+    currency: "usd"
+    card: stripeToken
+    description: email
+  }, (err, charge) ->
+    if err
+      console.log "There was an error"
+      console.log err
+    if err and err.type == "StripeCardError"
+      console.log "The card was declined"
+    else
+      console.log "The charge was made!"
+      console.log charge
   #res.redirect '/'
 
 
