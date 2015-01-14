7f62499de36c893773770b53e43a1e4495a64ec7
Lucy Richards <lucyannerichards@gmail.com>
2015-01-14 14:03:08 -0800

    render function in LoginPageView

--- public/javascripts/views/LoginPage.js
+++ public/javascripts/views/LoginPage.js
@@ -1,5 +1,15 @@
 
 (function(){
   $(document).ready(function() {
+    window.LoginPageView = Backbone.View.extend({
+      el: "#body",
+
+      render: function() {
+        var src = $("#login-template").html();
+        var template = Handlebars.compile(src);
+        var html = template();
+        this.$el.html(html);      
+      }
+    });
   });
 }).call(this);
