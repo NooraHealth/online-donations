97ac0ed65c0ab318d63ac34aac98761fd2945391
Lucy Richards <lucyannerichards@gmail.com>
2015-01-20 00:09:38 -0800

    renamed toDollars helper

--- public/js/templates/helpers/toDollars.js
+++ public/js/templates/helpers/toDollars.js
@@ -3,7 +3,7 @@
 Transforms a whole cent value into it's string equivalent in dollars.
 For example: 4200 - > $42.00
 **/
-define('templates/helpers/Dollars', ['hbs/handlebars'], function ( Handlebars ) {
+define('templates/helpers/toDollars', ['hbs/handlebars'], function ( Handlebars ) {
 
   function dollars ( amount, options ) {
     // The place where the '.' will go
@@ -15,6 +15,6 @@ define('templates/helpers/Dollars', ['hbs/handlebars'], function ( Handlebars ) 
 
   }
 
-  Handlebars.registerHelper( 'Dollars', dollars );
+  Handlebars.registerHelper( 'toDollars', dollars );
   return dollars;
 });
