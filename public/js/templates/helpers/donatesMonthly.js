544c11698debac6995ba00c8f9d0812ca10c0044
Lucy Richards <lucyannerichards@gmail.com>
2015-01-24 21:29:08 -0800

    donatesMonthly helper function

--- public/js/templates/helpers/donatesMonthly.js
+++ public/js/templates/helpers/donatesMonthly.js
@@ -6,8 +6,10 @@ For example: 4200 - > $42.00
 define('templates/helpers/donatesMonthly', ['hbs/handlebars'], function ( Handlebars ) {
 
   function donatesMonthly ( donor, options ) {
+    console.log("Thi is sht edonor in donatesMonthly");
     // The place where the '.' will go
-    return true;
+    if (donor == "testing")
+      return options.fn(this);
   }
 
   Handlebars.registerHelper( 'donatesMonthly', donatesMonthly );
