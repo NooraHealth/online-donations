0546d70facc8f07470ee83cf9213ef4f96955eec
lucyrichards <lucyannerichards@gmail.com>
2015-02-02 14:07:16 -0800

    payment info on account info page

---
+++ public/js/templates/helpers/getCardInfo.js
@@ -0,0 +1,25 @@
+
+
+/**
+Returns whether the donor donates monthly. If the donor is a one-time donor, 
+the function returns false
+**/
+define('templates/helpers/getCardInfo', ['hbs/handlebars'], function ( Handlebars ) {
+
+  function cardInfo ( donor, options ) {
+    
+    if (!donor.cards)
+      return "";
+
+    if (donor.cards.total_count == 0)
+      return "";
+
+    console.log(donor);
+    var card =  donor.cards.data[0];
+    return card.brand + " " + "**** **** **** " + card.last4;
+    
+  }
+
+  Handlebars.registerHelper( 'getCardInfo', cardInfo );
+  return cardInfo;
+});
