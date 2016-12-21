b5a43ce8474870d8dc048d3f12cca4599484c4c7
lucyannerichards@gmail.com <lucyannerichards@gmail.com>
2016-12-21 11:22:57 -0800

    form fields submitting properly to stripe client side again

--- public/js/views/DonationPage/DonationFormView.js
+++ public/js/views/DonationPage/DonationFormView.js
@@ -3,8 +3,8 @@ define([
   'jquery',     // lib/jquery/jquery
   'underscore', // lib/underscore/underscore
   'backbone',    // lib/backbone/backbone
-  'handlebars',   
-  'stripe',   
+  'handlebars',
+  'stripe',
   'hbs!templates/donationForm',
   'views/MessageView',
   'models/Message',
@@ -13,9 +13,9 @@ define([
 ], function($, _, Backbone, Handlebars, Stripe, donationFormTemplate,  MessageView, Message, Donation, FormBase ){
 
     var DonationForm = FormBase.extend({
-    
+
       el: "#body",
-      
+
       donationBox: function() {
         return $("input[name=amount]");
       },
@@ -23,7 +23,7 @@ define([
       submitDonation: function () {
         return this.$('#submit-donation');
       },
-      
+
 
       initialize: function(options) {
         this.donor = options.donor;
@@ -54,7 +54,7 @@ define([
         e.preventDefault();
         this.donationBox().val(e.target.value);
       },
-      
+
       /*
        * Parse expiration input for use by Stripe
        */
@@ -65,21 +65,23 @@ define([
 
         var month = exp.substring(0,2);
         var year = exp.substring(3);
-        
+
         $("#expyear").val(year);
         $("#expmonth").val(month);
       },
-      
+
       /*
        * Callback for Stripe.card.createToken(...)
        */
       stripeResponseHandler: function(status, response){
+        console.log(response);
+        console.log(status);
         if(response.error) {
           this.message.set({error: response.error.message});
           this.disableSubmitButton(false);
         } else{
           var token = response.id;
-          
+
           var data = {
             stripeToken: token,
             name: $("input[name=name]").val(),
@@ -88,7 +90,7 @@ define([
             email: $("input[name=email]").val(),
             amount: $("input[name=amount]").val() * 100,
             monthly: $("input[name=monthly]").is(':checked') ,
-            newsletter: $("input[name=newsletter]").is(':checked') 
+            newsletter: $("input[name=newsletter]").is(':checked')
           }
 
           this.disableSubmitButton(true);
@@ -98,10 +100,13 @@ define([
       },
 
       handleResponse: function(model,response) {
+        console.log("in rthe handle response");
         if ( response.error ) {
           this.handleServerError(response.error);
-        } 
+        }
         if ( response.donor ) {
+          console.log("the response");
+          console.log(response);
           this.donor.set( response.donor );
           this.donor.set( {count: response.count} );
           this.donor.set({donations: response.donations.data});
@@ -117,13 +122,16 @@ define([
        */
       createStripeToken: function(event) {
         event.preventDefault();
+        console.log("creating stripe token");
         var donation = new Donation();
         this.parseExpiration();
         this.$("#submit-donation").prop('disabled', true);
+        //hack to deal with hidden form fields with data-stripe
+        $("div:hidden").remove();
         Stripe.card.createToken(this.form(), (this.stripeResponseHandler).bind(this));
 
         //Prevent form from submitting
-        return false; 
+        return false;
       },
 
       //reset form fields
@@ -136,21 +144,21 @@ define([
         console.log("Rendering initialLoad");
         if( !this.initialLoad ) {
           var html = donationFormTemplate();
-          this.$el.html(html);      
+          this.$el.html(html);
           this.initialLoad = false;
         }
 
         //Set the el of the form message view now that the form has been rendered
         this.message = new Message();
-        this.messageView = new MessageView({model: this.message, el: $("#form-message")}); 
+        this.messageView = new MessageView({model: this.message, el: $("#form-message")});
         this.messageView.render();
-        
+
         if (this.donor.isLoggedIn()) {
           //disable the form submit button so no new donor can be created while loggedIn
           this.disableSubmitButton(true);
           this.message.set({error: "Please log out to register a new donor"});
         }
-        
+
         return this;
       }
     });
